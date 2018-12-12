93b9a035429cf327db89b7e4e787ff08c52d5c08
hilmifr22 <hilmi.fr98@gmail.com>
2018-12-13 01:45:37 +0700

    profil

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -8,16 +8,22 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <style>
-
+.zuzu {
+  vertical-align: middle;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
 </style>
 </head>
 
 <body>
   &nbsp <a href="/home" class="btn btn-primary">Kembali</a>
     <br><br>
-    &nbsp <img src="/images/img_avatar.png" alt="Avatar" class="avatar">
-&nbspNama : {{ Auth::user()->name }}<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-&nbspEmail : {{ Auth::user()->email }}<br>
+            <center><img src="/images/img_avatar.png" alt="Avatar" style="width: 150px; height: 150px; border-radius: 50%;">
+            <br><br><h1>{{ Auth::user()->name }}
+            <br>{{ Auth::user()->email }}</h1></center>
+
 <script>
 
 </script>
